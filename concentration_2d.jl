a3a7a92423ed6e4af11205ccfadc51a01d707dc2
shanalyb <go.denis.28@yandex.ru>
2020-03-05 01:50:16 +0300

    add concentration

--- concentration_2d.jl
+++ concentration_2d.jl
@@ -1,27 +1,43 @@
-function concentration_2d(c_0, N_x, N_y, h_x, h_y, u_x, u_y, delta_t)
-    dif_c_x = grad2D_x(c_0, h_x)
-    dif_c_y = grad2D_y(c_0, h_y)
-    c = zeros(N_x+1, N_y+1)
+function concentration_2d(c_0::Array, N_x::Int64, N_y::Int64, h_x::Float64, h_y::Float64, u_x::Array, u_y::Array, t::Int64)
+    for i = 1:t
+        c_x = zeros(N_x + 1, N_y)
+        c_y = zeros(N_x, N_y + 1)
+        dif_c_x = grad2D_x(c_0, h_x)
+        dif_c_y = grad2D_y(c_0, h_y)
 
-    dif_c_x1 = zeros(N_x+1, N_y+1)
-    for i = 1:N_x+1, j = 1:N_y
-        dif_c_x1[i,j] = dif_c_x[i,j]
+        dif_c_x1 = zeros(N_x + 1, N_y + 1)
+        for i = 1:N_x+1, j = 1:N_y
+            dif_c_x1[i, j+1] = dif_c_x[i, j]
+        end
+
+        dif_c_y1 = zeros(N_x + 1, N_y + 1)
+        for i = 1:N_x, j = 1:N_y+1
+            dif_c_y1[i, j] = dif_c_y[i, j]
+        end
+
+        for i = 1:N_x, j = 1:N_y
+            c_x[i, j] = u_x[i, j] * dif_c_x1[i, j]
+        end
+        for i = 1:N_x, j = 1:N_y
+            c_y[i, j] = u_y[i, j] * dif_c_y1[i, j]
+        end
+
+        c_x1 = zeros(N_x + 1, N_y + 1)
+        for i = 1:N_x+1, j = 1:N_y
+            c_x1[i, j] = c_x[i, j]
+        end
+
+        c_y1 = zeros(N_x + 1, N_y + 1)
+        for i = 1:N_x, j = 1:N_y+1
+            c_y1[i, j] = c_y[i, j]
+        end
+
+        c_0 = c_0 - (c_x1 + c_y1) * t
     end
 
-    dif_c_y1 = zeros(N_x+1, N_y+1)
-    for i = 1:N_x, j = 1:N_y+1
-        dif_c_y1[i,j] = dif_c_y[i,j]
+    c = zeros(N_x, N_y)
+    for i = 1:N_x, j = 1:N_y
+        c[i, j] = c_0[i, j]
     end
-
-    u_x1 = zeros(N_x+1, N_y+1)
-    for i = 1:N_x+1, j = 1:N_y
-        u_x1[i,j] = u_x[i,j]
-    end
-
-    u_y1 = zeros(N_x+1, N_y+1)
-    for i = 1:N_x, j = 1:N_y+1
-        u_y1[i,j] = u_y[i,j]
-    end
-
-    c = c_0 - (u_y1 * dif_c_x1 + u_x1 * dif_c_y1) * delta_t
+    return c
 end
--- main_2d.jl
+++ main_2d.jl
@@ -1,19 +1,26 @@
-using Statistics, PyPlot, PyCall
-@pyimport scipy.optimize as so
-import PyPlot: quiver, colorbar, imshow
+using PyCall, Statistics
+@pyimport matplotlib.animation as anim
+using PyPlot
 pygui(true)
 include("gradient.jl")
+include("concentration_2d.jl")
 
-N_x = 10; N_y = 10
-L_x = 10; L_y = 10 #Расстояние (м)
-P_left = 4; P_right = 2;
-P_up = 4; P_down = 4;
-h_x = L_x / N_x; h_y = L_y / N_y
-dx = 10 / N_x; dy = 10 / N_y
-#k = rand(Float64, N_x + 2, N_y + 2)
+N_x = 10;
+N_y = 10;
+L_x = 10; #Расстояние (м)
+L_y = 10; #Расстояние (м)
+P_left = 4;
+P_right = 2;
+P_up = 4;
+P_down = 4;
+h_x = L_x / N_x;
+h_y = L_y / N_y;
+dx = 10 / N_x;
+dy = 10 / N_y;
+k = rand(Float64, N_x + 2, N_y + 2)
 mu = 10^(-3)
 h = 0.01
-k = 0.5
+#k = 0.5
 
 
 
@@ -26,37 +33,38 @@ p_min = zeros(N_y + 1, 1)
 p_max = zeros(N_y + 1, 1)
 p_ex = zeros(1, (N_x + 1) * (N_y + 1))
 
-for i = 1:N_x+1, j = 1:N_x+1
-    a_old[i, j] = (k / h_x)
-end
-
-for i = 1:N_y+1, j = 1:N_y+1
-    b_old[i, j] = (k / h_y)
-end
-
-#for i = 2:N_x+2, j = 1:N_x+1
-#    a_old[i-1, j] = (h_y / (k[i, j] - k[i-1, j])) * (log(k[i, j] / k[i-1, j]))
+#for i = 1:N_x+1, j = 1:N_x+1
+#    a_old[i, j] = (k / h_x)
 #end
 
-#for i = 1:N_y+1, j = 2:N_y+2
-#    b_old[i, j-1] = (h_x / (k[i, j] - k[i, j-1])) * (log(k[i, j] / k[i, j-1]))
-#end1
+#for i = 1:N_y+1, j = 1:N_y+1
+#    b_old[i, j] = (k / h_y)
+#end
 
+#Неоднородная среда_____________
+for i = 2:N_x+2, j = 1:N_x+1
+    a_old[i-1, j] = (h_y / (k[i, j] - k[i-1, j])) * (log(k[i, j] / k[i-1, j]))
+end
+
+for i = 1:N_y+1, j = 2:N_y+2
+    b_old[i, j-1] = (h_x / (k[i, j] - k[i, j-1])) * (log(k[i, j] / k[i, j-1]))
+end
+#______________________________
 
 for m = 1:(N_y-1)
     A[m, m+1] = 1
-    A[N_y-1+m, N_x*(N_y+1)+m+1] = 1
+    A[N_y-1+m, N_x * (N_y + 1) + m + 1] = 1
     b[m, 1] = P_left
     b[N_y-1+m, 1] = P_right
 end
 
 for m = 0:N_x
     A[2*(N_y-1)+(m+1), m*(N_y+1)+1] = 1
-    A[2*(N_y-1)+(N_x+1)+(m+1), m*(N_y+1)+(N_y+1)] = 1
+    A[2 * (N_y - 1) + (N_x + 1) + (m + 1), m*(N_y+1)+(N_y+1)] = 1
     #b[2*(N_y-1)+(m+1), 1] = P_up
     #b[2*(N_y-1)+(N_x+1)+(m+1), 1] = P_down
-    A[2*(N_y-1)+(m+1), m*(N_y+1)+1+1] = -1
-    A[2*(N_y-1)+(N_x+1)+(m+1), m*(N_y+1)+(N_y+1)-1] = -1
+    A[2*(N_y-1)+(m+1), m * (N_y + 1) + 1 + 1] = -1
+    A[2 * (N_y - 1) + (N_x + 1) + (m + 1), m*(N_y+1)+(N_y+1)-1] = -1
 end
 
 m = 2 * (N_x + N_y)
@@ -80,26 +88,54 @@ for j = 1:N_y+1, i = 1:N_x+1
     p[i, j] = p_old[n]
 end
 
-fig = imshow(p, extent=[0,L_x,0,L_y], interpolation = "gaussian")
-#fig.set_xlabel("X")
-cbar = colorbar(fig)
-cbar.set_label("P, MPa", fontsize=14)
+#fig = imshow(p, extent = [0, L_x, 0, L_y], interpolation = "gaussian")
+#cbar = colorbar(fig)
+#cbar.set_label("P, MPa", fontsize = 14)
 
-u_x = - (k / mu) * grad2D_x(p, h_x)
-u_y = - (k / mu) * grad2D_y(p, h_y)
+#Неоднородная среда_____________
+k_x = zeros(N_x, N_y + 1)
+for i = 1:N_x, j = 1:N_y+1
+    k_x[i, j] = k[i, j]
+end
+k_y = zeros(N_x + 1, N_y)
+for i = 1:N_x+1, j = 1:N_y
+    k_y[i, j] = k[i, j]
+end
+#_______________________________
+u_x = zeros(N_x + 1, N_y)
+u_y = zeros(N_x, N_y + 1)
+for i = 1:N_x+1, j = 1:N_y
+    u_x[i, j] = -(k[i, j] / mu) * grad2D_x(p, h_x)[i, j] * 10^(-6)
+end
+for i = 1:N_x, j = 1:N_y+1
+    u_y[i, j] = -(k[i, j] / mu) * grad2D_y(p, h_y)[i, j] * 10^(-6)
+end
 
-#x = zeros(L_x, L_y), y = zeros(L_x, L_y)
+#quiver(u_x, u_y)
 
-#function numpy_mgrid(dim1,dim2)
-#    X = [i for i in 1:dim1, j in 1:dim2]
-#    Y = [j for i in 1:dim1, j in 1:dim2]
-#    return X,Y
+#Концентрация
+c_0 = zeros(N_x + 1, N_y + 1)
+for i = 1:N_x+1
+    c_0[i, 1] = 1
+end
+#Неоднородная среда_____________
+#c_0 = zeros(N_x, N_y)
+#for i = 1:N_x
+#    c_0[i, 1] = 1
 #end
+#u_x_si_inhomogenes = zeros(N_x, N_y-1)
+#for i = 1:N_x, j = N_y-1
+#    u_x_si_inhomogenes[i, j] = u_x_si[i, j]
+#end
+#u_y_si_inhomogenes = zeros(N_x-1, N_y)
+#for i = 1:N_x-1, j = 1:N_y
+#    u_y_si_inhomogenes[i, j] = u_y_si[i, j]
+#end
+#_______________________________
 
-#x, y = numpy_mgrid(L_x, L_y)
-#x, y = 0.5*x, 0.5*y
-#x = linspace(0,1,L_x)
-#y = linspace(1,0,L_y)
-#x = range(0, stop=1, length=L_x)
-#y = range(1, stop=0, length=L_y)
-quiver(u_x, u_y)
+t = 200
+c = concentration_2d(c_0, N_x, N_y, h_x, h_y, u_x, u_y, t)
+
+fig = imshow(c, extent = [0, L_x + 1, 0, L_y + 1], interpolation = "gaussian")
+cbar = colorbar(fig)
+cbar.set_label("Concentration", fontsize = 14)
