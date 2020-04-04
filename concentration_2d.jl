07ee2c8d95198cb8f66a16cb1be23610a5b42792
shanalyb <go.denis.28@yandex.ru>
2020-04-04 05:00:30 +0300

    fix

--- concentration_2d.jl
+++ concentration_2d.jl
@@ -1,4 +1,6 @@
-function concentration_2d(c_0::Array, N_x::Int64, N_y::Int64, h_x::Float64, h_y::Float64, u_x::Array, u_y::Array, t::Int64)
+function concentration_2d(c_0::Array, h_x::Float64, h_y::Float64, u_x::Array, u_y::Array, t::Int64)
+    N_x = size(c_0, 1) - 1
+    N_y = size(c_0, 2) - 1
     for i = 1:t
         c_x = zeros(N_x + 1, N_y)
         c_y = zeros(N_x, N_y + 1)
@@ -6,12 +8,12 @@ function concentration_2d(c_0::Array, N_x::Int64, N_y::Int64, h_x::Float64, h_y:
         dif_c_y = grad2D_y(c_0, h_y)
 
         dif_c_x1 = zeros(N_x + 1, N_y + 1)
-        for i = 1:N_x+1, j = 1:N_y
+        for i = 1:N_x, j = 1:N_y
             dif_c_x1[i, j+1] = dif_c_x[i, j]
         end
 
         dif_c_y1 = zeros(N_x + 1, N_y + 1)
-        for i = 1:N_x, j = 1:N_y+1
+        for i = 1:N_x, j = 1:N_y
             dif_c_y1[i, j] = dif_c_y[i, j]
         end
 
